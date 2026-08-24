3d2af5ced24edff8f78c1aa2a45b7233d7e3e6de
jihwangim09-design <jihwangim09@gmail.com>
2026-08-24 16:20:54 +0900

    24일 취합 1차

--- miniproject2/model/dao/BaseDAO.java
+++ miniproject2/model/dao/BaseDAO.java
@@ -1,7 +1,26 @@
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class BaseDAO {
-    String URL = "jdbc:mysql://localhost:3306/본인DB이름";
+    String URL = "jdbc:mysql://localhost:3306/SmartLocker";
     String USER = "root";
-    String PASSWORD = "본인DB비밀번호";
+    String PASSWORD = "1234";
+
+    protected Connection conn;
+
+    private void connect() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("--> DB 연동 성공!"); 
+        } catch(Exception e) {
+            System.out.println("--> DB 연동 실패: " + e);
+        }
+    }
+    protected BaseDAO() { 
+        connect(); 
+    }
 }
+
