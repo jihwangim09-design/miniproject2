5c8b67dd6898ecacd13052dc0488825b71761aa9
jihwangim09-design <jihwangim09@gmail.com>
2026-08-25 15:52:42 +0900

    Users con dao dto 수정

--- miniproject2/model/dao/UsersDAO.java
+++ miniproject2/model/dao/UsersDAO.java
@@ -16,15 +16,14 @@ public class UsersDAO extends BaseDAO {
         boolean result = false; // 기본값 false
         try{
             // 회원 정보 추가
-            String sql = "insert into Users (u_id, u_pwd, u_phone, u_name, u_student_id) values (?, ?, ?, ?, ?)";
+            String sql = "insert into Users ( u_pwd, u_phone, u_name, u_student_id) values (?, ?, ?, ?)";
             // PreparedStatement 객체 생성
             PreparedStatement ps = conn.prepareStatement(sql);
 
-            ps.setString(1, dto.getU_id());
-            ps.setString(2, dto.getU_pwd());
-            ps.setString(3, dto.getU_phone());
-            ps.setString(4, dto.getU_name());
-            ps.setString(5, dto.getU_student_id());
+            ps.setString(1, dto.getU_pwd());
+            ps.setString(2, dto.getU_phone());
+            ps.setString(3, dto.getU_name());
+            ps.setString(4, dto.getU_student_id());
             int rows = ps.executeUpdate(); // DB에 들어간 줄(row) 수 확인 성공하면 1 실패시 0
             result = (rows == 1); // 위에서 제대로 실행 됐으면 true 아닐시 false 
 
@@ -35,35 +34,29 @@ public class UsersDAO extends BaseDAO {
     } // signup end
 
     // 로그인
-    public UsersDTO login( UsersDTO dto ) {
-        UsersDTO loginUser = null; // 기본값 null
-        try{
-            String sql = "SELECT * FROM Users WHERE u_student_id = ? AND u_pwd = ? ";
-            PreparedStatement ps = conn.prepareStatement(sql);
-
-            ps.setString(1, dto.getU_student_id());
-            ps.setString(2, dto.getU_pwd());
-
-            ResultSet rs = ps.executeQuery(); // insert는 update지만 select는 query를 사용
-
-            if(rs.next()) {
-                loginUser = new UsersDTO(
-                    rs.getInt("u_no"),
-                    rs.getString("u_id"),
-                    rs.getString("u_pwd"),
-                    rs.getString("u_phone"),
-                    rs.getString("u_name"),
-                    rs.getString("u_grade"),
-                    rs.getString("u_student_id")
-                );
-            }
-
-            rs.close();
-            ps.close();
-
-        }catch( Exception e ) { e.printStackTrace(); }
-        return loginUser; // 성공하면 유저 정보 실패하면 null
-    }
+    public UsersDTO login(String studentId, String pwd) {
+    UsersDTO loginUser = null;
+    try{
+        String sql = "SELECT * FROM Users WHERE u_student_id = ? AND u_pwd = ? ";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, studentId);
+        ps.setString(2, pwd);
+        ResultSet rs = ps.executeQuery();
+        if(rs.next()) {
+            loginUser = new UsersDTO(
+                rs.getInt("u_no"),
+                rs.getString("u_pwd"),
+                rs.getString("u_phone"),
+                rs.getString("u_name"),
+                rs.getString("u_grade"),
+                rs.getString("u_student_id")
+            );
+        }
+        rs.close();
+        ps.close();
+    }catch( Exception e ) { e.printStackTrace(); }
+    return loginUser;
+}
 
     // 학번 유효성 검사
     public boolean checkStudentId( String studentId ) {
@@ -103,25 +96,6 @@ public class UsersDAO extends BaseDAO {
         return result;
     }
 
-    // 아이디 중복 확인
-    public boolean checkID( String id ) {
-        boolean result = true;
-        try{
-            String sql = "SELECT * FROM Users WHERE u_id = ? ";
-            PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setString(1, id);
-            ResultSet rs = ps.executeQuery();
-            
-            if (rs.next()) {
-                result = false;
-            }
-
-            rs.close();
-            ps.close();
-        } catch ( Exception e ) { e.printStackTrace(); }
-        return result;
-    }
-
     // 회원 상세 조회
     public UsersDTO getUserInfo( int u_no ) {
         UsersDTO user = null;
@@ -134,7 +108,6 @@ public class UsersDAO extends BaseDAO {
             if (rs.next()) {
                 user = new UsersDTO(
                     rs.getInt("u_no"),
-                    rs.getString("u_id"),
                     rs.getString("u_pwd"),
                     rs.getString("u_phone"),
                     rs.getString("u_name"),
@@ -150,7 +123,6 @@ public class UsersDAO extends BaseDAO {
         return user;
     }
 
-
     // 전체회원조회(관리자용)
     public ArrayList<UsersDTO> selectAllUsers() {
         ArrayList<UsersDTO> list = new ArrayList<>();
@@ -162,7 +134,6 @@ public class UsersDAO extends BaseDAO {
             while (rs.next()) {
                 UsersDTO user = new UsersDTO(
                     rs.getInt("u_no"),
-                    rs.getString("u_id"),
                     rs.getString("u_pwd"),
                     rs.getString("u_phone"),
                     rs.getString("u_name"),
