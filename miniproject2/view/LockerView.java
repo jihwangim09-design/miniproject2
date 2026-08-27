8d1e68d3465cfc29463503e7a1872a974816a772
jihwangim09-design <jihwangim09@gmail.com>
2026-08-27 18:08:31 +0900

    18:08

--- miniproject2/view/LockerView.java
+++ miniproject2/view/LockerView.java
@@ -1,5 +1,7 @@
 package view;
 
 public class LockerView {
-    
+    private LockerView(){}
+    private static final LockerView instance = new LockerView();
+    public static LockerView getInstance() { return instance; }
 }
