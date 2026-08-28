75f1a6667f22702676c02a2708fc932d6fa7bfd9
Hyeongjin3581 <poll3581@naver.com>
2026-08-28 13:23:57 +0900

    123

--- miniproject2/controller/StudentController.java
+++ miniproject2/controller/StudentController.java
@@ -39,6 +39,13 @@ public class StudentController {
         return equipmentDAO.e_available();
     }
 
+    // 장비 상태 변경
+    public boolean e_statusupdate(int e_No, String e_Status){
+
+        return equipmentDAO.e_statusupdate(e_No, e_Status);
+    }
+
+
 
     // 보관함 조회
 
